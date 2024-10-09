4be2305bd2d85aef08fb031175d0061ca51beaad
C1yde11B <Clydellb0@gmail.com>
2024-10-09 08:11:01 -0500

    Initial Commit

---
+++ StudyCards/StudyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCardsApp.swift
+//  StudyCards
+//
+//  Created by AM Student on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyCardsTests/StudyCardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCardsTests.swift
+//  StudyCardsTests
+//
+//  Created by AM Student on 10/9/24.
+//
+
+import Testing
+@testable import StudyCards
+
+struct StudyCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
